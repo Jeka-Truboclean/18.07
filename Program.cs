45ef533303cdac4c6d388ba354aea1e280d4bf0e
Eugen Karaman <karamanjekakol@gmail.com>
2024-07-16 17:15:33 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace _18._07
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string fileName = "file.txt";
+            using (FileStream fs = File.Create(fileName))
+            {
+                byte[] info = new UTF8Encoding(true).GetBytes(fileName);
+                fs.Write(info, 0, info.Length);
+            }
+
+            using (StreamReader st = File.OpenText(fileName))
+            {
+                string s = "";
+                while ((s = st.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+    }
+}
